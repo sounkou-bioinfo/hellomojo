7407f393878d0b02098c8a72deba59decd836ee4
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-10-26 22:21:53 +0100

    Call

--- src/RC_hellomojo.c
+++ src/RC_hellomojo.c
@@ -1,8 +1,6 @@
 #include <R.h>
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
-#include <stdio.h>
-#include <stdlib.h>
 // Declaration of the Mojo function
 extern void hello();
 extern double add(double a, double b);
@@ -15,21 +13,8 @@ SEXP add_call(SEXP a, SEXP b) {
 }
 // .Call wrapper for hello 
 SEXP hello_call() {
-    char tmpname[L_tmpnam];
-    tmpnam(tmpname);
-    FILE *tmp = freopen(tmpname, "w+", stdout);
-    if (!tmp) {
-        error("Failed to redirect stdout");
-    }
     hello();
-    fflush(stdout);
-    freopen("/dev/tty", "w", stdout); // restore stdout
-    fseek(tmp, 0, SEEK_SET);
-    char buffer[4096];
-    size_t n = fread(buffer, 1, sizeof(buffer) - 1, tmp);
-    buffer[n] = '\0';
-    fclose(tmp);
-    return Rf_mkString(buffer);
+    return R_NilValue;
 }
 static const R_CallMethodDef CallEntries[] = {
     {"hello", (DL_FUNC) &hello_call, 0},
